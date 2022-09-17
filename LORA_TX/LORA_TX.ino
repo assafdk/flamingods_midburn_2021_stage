d97dbf2eef28ab539b450d1aba373930067e11f3
Itai Ferdman <itai@emmasensing.com>
2022-09-17 21:06:17 +0300

    Added test programs for sending and receiving LoRa

---
+++ LORA_TX/LORA_TX.ino
@@ -0,0 +1,36 @@
+#include <SPI.h>
+#include "LoRa.h"
+#include <Wire.h>
+#define OLED_RESET 9
+int counter = 0;
+
+void setup() {
+  //   rtc.begin();
+  // rtc.adjust(DateTime(2020, 11, 5, 11, 14, 50));
+
+  delay(100);
+
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("LoRa Sender");
+
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  // send packet
+  //return;
+  for (int i = 0; i < 500; i++) {
+    Serial.println("Sending data");
+    uint8_t data[8] = {'0', '1', '0', '0', '1', '0', '0', '\0'};
+    LoRa.beginPacket();
+    LoRa.write(data, 8);
+    LoRa.endPacket(true);
+  }
+
+
+}
